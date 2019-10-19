5be378ee9ae77176ef57734547f9c7f632e04c78
Phumthawan <knzphumthawan@gmail.com>
2019-10-19 15:54:14 +0700

    add footer

--- src/components/layouts/footer.js
+++ src/components/layouts/footer.js
@@ -1,26 +1,27 @@
 import React from 'react'
+import {
+    Jumbotron
+} from 'reactstrap'
 
 const footer = () => {
     const date = new Date()
     const year = date.getFullYear();
     return (
         <footer>
-            <div className="jumbotron">
-                <div className="lead text-center">
-                    Copyright &copy; {year == 2019 ? year : `2019 - ${year}`} Mersock Blogger. All rights reserved.
-            </div>
-            </div>
+            <Jumbotron>
+                <p class="mb-1"> Copyright &copy; {year == 2019 ? year : `2019 - ${year}`} Mersock Blogger. All rights reserved.</p>
+            </Jumbotron>
             <style jsx>{`
-                 footer {
+                footer {
                     left: 0;
                     bottom: 0;
                     width: 100%;
                     text-align: center;
-                    margin-top:100px
-                 }
-            `}
+                    margin-top:30px
+                }
+                `}
             </style>
-        </footer>
+        </footer >
     )
 }
 
